93ffad32d3a3ef37a3f5f9fcee03a5dc88303b19
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-08-18 13:38:23 -0400

    changed domain in cors and session store.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  url = Rails.env == 'production' ? 'https://arn-tracking-app.herokuapp.com' : 'http://localhost:3000'
+  url = Rails.env == 'production' ? 'https://arn-gdt-assessment.herokuapp.com' : 'http://localhost:3000'
   allowed_methods = %i[get post put patch delete options head]
   allow do
     origins url
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,7 +4,7 @@ if Rails.env == 'production'
   Rails.application
        .config.session_store :cookie_store,
                              key: '_authentication_app',
-                             domain: 'arn-tracking-app-api.herokuapp.com'
+                             domain: 'arn-gdt-assessment-api.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store,
                                          key: '_authentication_app'
