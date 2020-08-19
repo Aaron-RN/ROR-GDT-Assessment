81c43f66fd5578bfa2bac0bac036f1245b6c44c2
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-08-19 14:34:15 -0400

    Set credentials to false

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,6 +6,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allowed_methods = %i[get post put patch delete options head]
   allow do
     origins url
-    resource '*', headers: :any, methods: allowed_methods, credentials: true
+    resource '*', headers: :any, methods: allowed_methods, credentials: false
   end
 end
