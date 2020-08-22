faae985c58a4e57586d2d5e2c3bd23d8a4b9e079
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-08-22 17:00:25 -0400

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allowed_methods = %i[get post put patch delete options head]
   allow do
     origins url
-    resource '*', headers: :any, methods: allowed_methods, credentials: false
+    resource '*', headers: :any, methods: allowed_methods, credentials: true
   end
 end
