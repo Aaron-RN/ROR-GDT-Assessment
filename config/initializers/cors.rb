59293ae8a58b88579dcfcbc31698af35b7bc5461
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-08-22 16:47:21 -0400

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-# url = Rails.env == 'production' ? 'https://arn-gdt-assessment.herokuapp.com' : 'http://localhost:3000'
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  url = Rails.env == 'production' ? '*' : 'http://localhost:3000'
+  url = Rails.env == 'production' ? 'https://arn-gdt-assessment.netlify.app' : 'http://localhost:3000'
   allowed_methods = %i[get post put patch delete options head]
   allow do
     origins url
